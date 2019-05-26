9e01e61f43dce243295a83c0919adff4d82fe4c4
Ilya Bezkhodarnov <biv@tomsksoft.com>
2019-05-26 15:29:50 +0700

    Add cloning maintenance jobs

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ mkdir cis1
 
 git clone https://github.com/tomsksoft-llc/cis1-core-bash.git cis1
 git clone https://github.com/tomsksoft-llc/cis1-libs-py.git cis1/libs
-git clone  https://github.com/vasilek-ilya/apprentice-laravel cis1/webui
+git clone https://github.com/vasilek-ilya/apprentice-laravel cis1/webui
+git clone https://github.com/tomsksoft-llc/cis1-install-bashcore-laravelwebui-pythonlibs.git cis1/jobs/maintenance
