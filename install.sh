4834cd29c4b184a8d9f9da064d487885334d3002
Ilya Bezkhodarnov <biv@tomsksoft.com>
2019-05-26 14:22:50 +0700

    Install scripts added

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf cis1
+mkdir cis1
+
+git clone https://github.com/tomsksoft-llc/cis1-core-bash.git cis1
+git clone https://github.com/tomsksoft-llc/cis1-libs-py.git cis1/libs
+git clone  https://github.com/vasilek-ilya/apprentice-laravel cis1/webui
